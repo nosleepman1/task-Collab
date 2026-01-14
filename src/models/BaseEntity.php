a7f14f223a096d81c246fd10cff76b67958ffab1
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-01-14 10:40:09 +0000

    base parents classes

---
+++ src/models/BaseEntity.php
@@ -0,0 +1,68 @@
+<?php 
+    namespace App\Models;
+
+use DateTime;
+
+    abstract class BaseEntity {
+
+        private ?int $id = null;
+        private ?DateTime $createdAt = null;
+        private ?DateTime $updatedAt = null;
+
+        public function __construct() {
+            if ($this->createdAt == null) {
+                $this->createdAt = new DateTime();
+            }
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getCreatedAt() {
+            return $this->createdAt;
+        }
+
+        public function getUpdatedAt() {
+            return $this->updatedAt;
+        }
+
+        public function setId(int $id) :self{
+            $this->id = $id;
+            return $this;
+        }
+
+        public function setCreatedAt(DateTime $date): self  {
+            $this->createdAt = $date;
+            return $this;
+        }
+
+        public function touch(DateTime $date): self  {
+            $this->updatedAt = new DateTime();
+            return $this;
+        }
+
+
+       
+
+
+        /**
+         * utils
+         */
+
+        public function exist() :bool{
+            return $this->id !== null;
+        }
+        
+
+        public function findOrFail(int $id) {
+            if ($id === null) {
+                throw new \Exception('Pas d\'ID');
+            } 
+            return $this->id;
+        }
+
+        abstract public function toArray() : array;
+    }
+   
+    
--- src/models/User.php
+++ src/models/User.php
@@ -1,25 +1,39 @@
 <?php 
+
     namespace App\models;
-    class User {
+    use App\Models\BaseEntity;
+    
+    class User extends BaseEntity {
        
         private $id;
-        private $username;
+        private string $firstname;
+        private string $lastname;
         private $email;
         private $password;
+        private ?bool $is_active = true;
+        private ?string $role = 'member' ;
+        
 
-        public function __construct($username, $email, $password, $id = null) {
-            $this->id = $id;
-            $this->username = $username;
+        public function __construct($firstname, $lastname, $email, $password) {
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
             $this->email = $email;
             $this->password = $password;
+            parent::__construct();
         }
+
+        
 
         public function getId() {
             return $this->id;
         }
 
-        public function getUsername() {
-            return $this->username;
+        public function getFirsname() {
+            return $this->firstname;
+        }
+
+        public function getLastname() {
+            return $this->lastname;
         }
 
         public function getEmail() {
@@ -29,4 +43,40 @@
         public function getPassword() {
             return $this->password;
         }
+
+        public function setPassword(string $password): self {
+            $this->password = password_hash($password, PASSWORD_BCRYPT);
+            return $this;
+        }
+
+         public function setPasswordHash(string $passwordHashed) : self{
+                $this->password = $passwordHashed;
+                return $this;
+         }
+
+
+         public function verifyPassword(string $password) : bool{
+                return $this->password = password_verify($this->password, $password);      
+         }
+
+         public function getFullname() {
+            return " {$this->firstname}  {$this->lastname}";
+         }
+
+
+         public function getInitials() {
+            $first = $this->firstname ? $this->firstname[0] : '';
+            $last = $this->lastname ? $this->lastname[0] : '';
+            return $first . $last;
+         }
+
+        
+         public function toArray(): array
+        {
+            return [
+                'id' => $this->id,
+                'createdAt' => $this->create
+            ];
+        }
+         
     }
